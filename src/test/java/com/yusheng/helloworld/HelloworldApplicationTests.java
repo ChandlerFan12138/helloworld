c344a1775df6c68606e879f44e39e2c50cf06255
ChandlerFan12138 <524592395@qq.com>
2022-09-29 11:26:54 +0100

    Initial commit

---
+++ src/test/java/com/yusheng/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusheng.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
